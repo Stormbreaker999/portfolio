1603a2e0735a5163f03de5660913d37f325f318e
Avichal <dubeyavichal999@gmail.com>
2024-09-21 19:16:07 +0530

    Changed profile photo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Layout from '../components/Layout'
-import profile from '../../public/developer-pic-1.png'
+import profile from '../../public/frontphoto.png'
 import Image from'next/image'
 import AnimatedText from '../components/AnimatedText'
 import Link from 'next/link'
